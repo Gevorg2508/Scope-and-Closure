ac039e2f4789e75b9fa84694a056e7f7bc216ad9
Harutyunyan Gevorg <gev.harutyunyan.96@gmail.com>
2021-12-06 21:50:02 +0400

    fix: i changed task 6

--- falsyFilter.6.js
+++ falsyFilter.6.js
@@ -1,12 +1,8 @@
 const values = [null, true, {}, { name: "Elon" }, "", NaN, 0];
 
 function falsyValueFilter(arr){
-    let result = [];
-    for (let i = 0; i<arr.length ;i++){
-        if(!!arr[i]){
-            result.push(arr[i]);
-        }
-    }
-    return result;
+    for(let i = 0 ; i< arr.length; i++){}
+        let res = arr.filter(e=>!!e);
+  return res;  
 }
 console.log(falsyValueFilter(values));
